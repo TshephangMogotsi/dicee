69a857544d43cdac7f5fe4ba4cfb71a2d90b6592
TshephangMogotsi <zinjie7@gmail.com>
2022-01-20 13:17:16 +0200

    displayed dice and wrapped them in a TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,29 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset("images/dice1.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset("images/dice3.png"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
